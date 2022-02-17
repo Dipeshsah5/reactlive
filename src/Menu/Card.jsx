135639c2570bfc65572538e0059f9031d9b0b1a0
Dipeshsah5 <dipesh.bay20@gmail.com>
2022-02-17 16:35:04 +0545

    my first commit

---
+++ src/Menu/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Sdata from "./Sdata";
+
+const Card = (props) => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img src={props.img} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-danger">
+          Learn More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
